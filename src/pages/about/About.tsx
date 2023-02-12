cbe212b0d5a0666bc93542b2d01276c6380ad960
ronnahom96 <ronnahom96@gmail.com>
2023-02-12 21:00:52 +0200

    about page

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './About.module.css'
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Twitch Chat</h1>
+      <p>
+        Twitch Chat is a live chat platform for Twitch streamers and viewers to
+        connect and engage in real-time conversations.
+      </p>
+      <p>
+        With Twitch Chat, you can join a live stream and participate in the chat
+        alongside other viewers and the streamer. You can also send and receive
+        messages, share your thoughts and opinions, and interact with others in
+        real-time.
+      </p>
+      <p>
+        Twitch Chat is designed to be fast, reliable, and easy to use, making it
+        the perfect tool for anyone looking to connect with others and enhance
+        their Twitch viewing experience.
+      </p>
+    </div>
+  );
+};
+
+export default About;
